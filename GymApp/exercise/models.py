ac10efc50d32a3294fffd637da43349114bad0d5
Krystian Kamyszek <kamyk@MacBook-Pro-Krystian.local>
2023-06-20 18:20:24 +0200

    create models of MuscleGroup Exercise Reps Workout and Trenig

--- GymApp/exercise/models.py
+++ GymApp/exercise/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class MuscleGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Exercise(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    muscle = models.ManyToManyField(MuscleGroup)
+    isRepEx = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.name
+    
+class Reps(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    reps = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.exercise.name} - Reps: {self.reps}"
+    
+
+class CircuitWorkout(models.Model):
+    exercises = models.ManyToManyField(Reps)
+    rest = models.IntegerField()
+    cicles = models.PositiveIntegerField()
+
+
+class SeriesWorkout(models.Model):
+    exercise = models.ForeignKey(Reps, on_delete=models.CASCADE)
+    series = models.PositiveIntegerField()
+
+
+class Traning(models.Model):
+    name = models.CharField(max_length=200)
+    workout = models.ManyToManyField(CircuitWorkout or SeriesWorkout) 
+    
+
+
+    
