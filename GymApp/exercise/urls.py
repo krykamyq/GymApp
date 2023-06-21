350d2fe4492fe7821cfcc387e31e9e4e6fdf7a12
Krystian Kamyszek <kamyk@MacBook-Pro-Krystian.local>
2023-06-21 18:45:11 +0200

    login and registration

--- GymApp/exercise/urls.py
+++ GymApp/exercise/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home , name='home')
+    path('', views.home , name='home'),
+    path('login', views.login_page, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('registration', views.registration_page, name='registration')
 ]
--- GymApp/exercise/views.py
+++ GymApp/exercise/views.py
@@ -1,8 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
-    return render(request , "main.html")
+    return render(request , "home.html")
  
+
+
+
+def login_page(request):
+
+    page = 'login'
+    context = {'page': page}
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = authenticate(request, username=username, password=password)
+        except:
+            messages.error(request, 'User does not exist.')
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the URL name for the home page
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return render(request, 'login.html',context)
+    
+    return render(request, 'login.html', context)
+    
+   
+def registration_page(request):
+    page = 'registration'
+    context = {'page': page}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'Username already exists.')
+                return render(request, 'login.html',context)
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                login(request, user)
+                return redirect('home')  # Replace 'home' with the URL name for the home page
+        else:
+            messages.error(request, 'Passwords do not match.')
+            return render(request, 'login.html',context)
+    
+    return render(request, 'login.html',context)
+
+    
+    
+
+def logout_user(request):
+     logout(request)
+     return redirect('home')
