ebfe352fa2c4ab1ffcec1768c0d31a62c248783a
Krystian Kamyszek <kamyk@MacBook-Pro-Krystian.local>
2023-06-21 21:19:22 +0200

    navbar and exercises list

--- GymApp/exercise/urls.py
+++ GymApp/exercise/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home , name='home'),
     path('login', views.login_page, name='login'),
     path('logout', views.logout_user, name='logout'),
-    path('registration', views.registration_page, name='registration')
+    path('registration', views.registration_page, name='registration'),
+
+
+    path('exercise', views.exercise, name = 'exercise')
 ]
--- GymApp/exercise/views.py
+++ GymApp/exercise/views.py
@@ -4,10 +4,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .models import *
 # Create your views here.
 
 def home(request):
     return render(request , "home.html")
+
+def exercise(request):
+    exercises = Exercise.objects.all()
+
+    context = {'exercises': exercises}
+    return render(request, 'exercise.html', context)
  
 
 
